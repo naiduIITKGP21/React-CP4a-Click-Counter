79bb0327a00f362dca75789a75a4c94319d5e554
Y A Naidu <naiduiitkgp21@gmail.com>
2022-04-03 14:17:43 +0000

    Error occuring

--- src/components/ClickCounter/index.js
+++ src/components/ClickCounter/index.js
@@ -1,1 +1,38 @@
 // Write your code here
+import './index.css'
+import {Component} from 'react'
+
+class ClickCounter extends Component {
+  state = {
+    count: 0,
+  }
+
+  clickCount = () => {
+    this.setState(prevState => ({count: prevState.count + 1}))
+  }
+
+  render() {
+    const {count} = this.state
+
+    return (
+      <div className="bg-container">
+        <div className="content-container">
+          <h1 className="heading">
+            The Button has been clicked
+            <span className="count">{count}</span> times
+          </h1>
+          <p className="description">click the button to increase the count</p>
+          <button
+            onClick={this.clickCount}
+            className="click-me-button"
+            type="button"
+          >
+            Click Me!
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ClickCounter
